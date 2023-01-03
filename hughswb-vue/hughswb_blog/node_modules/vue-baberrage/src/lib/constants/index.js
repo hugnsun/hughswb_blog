0c4195b6e3bb2f7cbc74fd95fac9c2c2a00ff2df
Admin <info@shblai.com>
2023-01-03 15:16:23 +0800

    update .gitignore

--- hughswb-vue/hughswb_blog/node_modules/vue-baberrage/src/lib/constants/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Configuration the Message type.
- */
-export const MESSAGE_TYPE = {
-  NORMAL: Symbol('NORMAL'), // Right to Left
-  FROM_TOP: Symbol('FROM_TOP'), // From Top
-  FROM_BOTTOM: Symbol('FROM_BOTTOM') // From Bottom
-}
