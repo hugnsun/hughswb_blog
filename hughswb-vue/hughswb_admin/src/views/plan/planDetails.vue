6ae97a312ba82717cfbd064843ffd9176188cf0c
Admin <info@shblai.com>
2023-02-02 14:25:31 +0800

    优化更改

--- hughswb-vue/hughswb_admin/src/views/plan/planDetails.vue
+++ hughswb-vue/hughswb_admin/src/views/plan/planDetails.vue
@@ -1,36 +1,48 @@
 <template>
     <div class="execution">
         <basic-container id="style-container">
-            <avue-crud ref="crud" :page.sync="page" :data="tableData" :table-loading="tableLoading"
-                :option="tableOption" @on-load="getList" @search-change="searchChange" @refresh-change="refreshChange"
-                @size-change="sizeChange" @current-change="currentChange" @row-update="handleUpdate"
-                @row-save="handleSave" @row-del="rowDel">
+            <avue-crud
+                ref="crud"
+                :page.sync="page"
+                :data="tableData"
+                :table-loading="tableLoading"
+                :option="tableOption"
+                @on-load="getList"
+                @search-change="searchChange"
+                @refresh-change="refreshChange"
+                @size-change="sizeChange"
+                @current-change="currentChange"
+                @row-update="handleUpdate"
+                @row-save="handleSave"
+                @row-del="rowDel"
+            >
                 <template slot="menuLeft">
-                    <vs-button upload @click="$refs.crud.rowAdd()">
-                        <i class="el-icon-plus" /> 新增计划
+                    <vs-button
+                        upload
+                        animation-type="vertical"
+                        style="width: 100px"
+                        @click="$refs.crud.rowAdd()"
+                    >
+                        <i class="el-icon-alitianjia1" /> 新增计划
+                        <template #animate
+                            ><i class="el-icon-alichukong" /> 点击新增
+                        </template>
                     </vs-button>
+                </template>
+                <template slot="delayOrNot" slot-scope="{ row }">
+                    <vs-switch danger v-model="row.delayOrNot" disabled>
+                        <template #off> 未延期 </template>
+                        <template #on> 延期 </template>
+                    </vs-switch>
+                </template>
+                <template slot="whetherToUploadNotes" slot-scope="{ row }">
+                    <vs-switch v-model="row.whetherToUploadNotes" disabled>
+                        <template #off> 未上传 </template>
+                        <template #on> 已上传 </template>
+                    </vs-switch>
+                </template>
 
-                </template>
-                <template slot="delayOrNot" slot-scope="{row}">
-                    <vs-switch danger v-model="row.delayOrNot == 1" disabled>
-                        <template #off>
-                            未延期
-                        </template>
-                        <template #on>
-                            延期
-                        </template>
-                    </vs-switch>
-                </template>
-                <template slot="whetherToUploadNotes" slot-scope="{row}">
-                    <vs-switch v-model="row.whetherToUploadNotes == 1" disabled>
-                        <template #off>
-                            未上传
-                        </template>
-                        <template #on>
-                            已上传
-                        </template>
-                    </vs-switch>
-                </template>
+                <template slot="menu"> </template>
             </avue-crud>
         </basic-container>
     </div>
@@ -51,6 +63,7 @@ export default {
                 pageSize: 20, // 每页显示多少条
             },
             tableLoading: false,
+
             tableOption: tableOption,
         };
     },
@@ -62,14 +75,15 @@ export default {
                 .then(({ data }) => {
                     this.tableData = data.data.records;
                     this.$notify({
-                        title: '成功',
-                        message: '数据查询成功',
-                        type: 'success'
+                        title: "成功",
+                        message: "数据查询成功",
+                        type: "success",
                     });
-                }).catch(() => {
+                })
+                .catch(() => {
                     this.$notify.error({
-                        title: '错误',
-                        message: '查询出现问题 请查询后端代码'
+                        title: "错误",
+                        message: "查询出现问题 请查询后端代码",
                     });
                 });
         },
@@ -101,9 +115,11 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-.el-icon-plus {
+ <style lang="scss" scoped>
+.el-icon-alitianjia1 {
+    padding-right: 5px;
+}
+.el-icon-alichukong {
     padding-right: 5px;
 }
 </style>
-
